f9a4944c49e5a0d17d059d2ab1dbb113f9016673
Maihoernchen <72254540+Maihoernchen@users.noreply.github.com>
2021-12-23 13:46:58 +0100

    gf

--- chooser.py
+++ chooser.py
@@ -5,39 +5,44 @@ konju = []
 vokab = []
 gramm = []
 
-d = open("dekli.txt", "r")
+d = open("C:\\Users\Atom\Documents\School fuckujd\latein\latin_stuff\dekli.txt", "r")
 for x in d:
-  dekli[x] = x
+  dekli.append(x)
 
-k = open("konju.txt", "r")
+k = open("C:\\Users\Atom\Documents\School fuckujd\latein\latin_stuff\konju.txt", "r")
 for x in k:
-  konju[x] = x
+  konju.append(x)
 
-v = open("vokab.txt", "r")
+v = open("C:\\Users\Atom\Documents\School fuckujd\latein\latin_stuff\\vokab.txt", "r")
 for x in v:
-  dekli[x] = x
+  vokab.append(x)
 
-g = open("gramm.txt", "r")
+g = open("C:\\Users\Atom\Documents\School fuckujd\latein\latin_stuff\gramm.txt", "r")
 for x in g:
-  dekli[x] = x
+  gramm.append(x)
 
-
+print(dekli,konju,gramm,vokab)
 def main(knew):
     if knew:
         questtype = random.randint(0,3)
         if questtype == 0:
-            question = dekli[random.randint(0,len(dekli)-1)]
+            a = len(dekli)-1
+            question = dekli[random.randint(0,a)]
             caption = "Welche Deklination ist das?"
-            answer
         elif questtype == 1:
+            a = len(konju)-1
             question = konju[random.randint(0,len(konju)-1)]
             caption = "Welche Konjugation ist das?"
         elif questtype == 2:
+            a = len(vokab)-1
             question = vokab[random.randint(0,len(vokab)-1)]
             caption = "Was ist die deutsche Überseztung von...?"
         else:
+            a = len(gramm)-1
             question = gramm[random.randint(0,len(gramm)-1)]
             caption = "Worum handelt es sich hier"
     else:
         question = question
-    return question, caption
+    return question, caption, answer
+
+print(main(True))

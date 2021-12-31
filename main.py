c1bb93c724e6c83f1c26ea5282505149d40981a9
Maihoernchen <maihoernchen.index@gmail.com>
2021-12-31 15:18:51 +0100

    changed stuff

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import gui as gui
 import chooser as choose
 import time
+import wrong_chooser as wrong
 
 knew = True
 
+question, caption, answer, questtype = choose.main()
+opt1,opt2,opt3,opt4,ans = wrong.main(questtype, answer)
+print(caption)
+print(question)
+print(opt1)
+print(opt2)
+print(opt3)
+print(opt4)
+print(ans)
+
 while True:
-    question, caption, answer = choose.main()
-    gui.main(caption, question, answer)
-    time.sleep(120)
+    if ans != gui.main(caption, question, opt1, opt2, opt3, opt4):
+        pass
+    else:
+        question, caption, answer, questtype = choose.main()
+        opt1,opt2,opt3,opt4,ans = wrong.main(questtype, answer)
+        print(caption)
+        print(question)
+        print(opt1)
+        print(opt2)
+        print(opt3)
+        print(opt4)
+        print(ans)
+    time.sleep(300)

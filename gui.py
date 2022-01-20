dec7ded756560e25dc517aa9817072dfc49c425a
Maihoernchen <maihoernchen.index@gmail.com>
2022-01-20 12:20:56 +0100

    duck

--- gui.py
+++ gui.py
@@ -1,7 +1,21 @@
 import pygame
 # fuckukyle
 
+def button(screen, position, text):
+    font = pygame.font.SysFont("Arial", 50)
+    text_render = font.render(text, 1, (255, 0, 0))
+    x, y, w , h = text_render.get_rect()
+    x, y = position
+    pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w , y), 5)
+    pygame.draw.line(screen, (150, 150, 150), (x, y - 2), (x, y + h), 5)
+    pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w , y + h), 5)
+    pygame.draw.line(screen, (50, 50, 50), (x + w , y+h), [x + w , y], 5)
+    pygame.draw.rect(screen, (100, 100, 100), (x, y, w , h))
+    return screen.blit(text_render, (x, y))
+
 def main(caption, question, opt1, opt2, opt3, opt4):
+    b1 = button(screen, (400, 300), opt2)
+    b2 = button(screen, (500, 300), opt1)
     z = len(question)
     WIDTH, HEIGHT = 100+45*z,100+35*z
     WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -42,5 +56,10 @@ def main(caption, question, opt1, opt2, opt3, opt4):
                     ans = 4
                 else:
                     break
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if b1.collidepoint(pygame.mouse.get_pos()):
+                    pygame.quit()
+                elif b2.collidepoint(pygame.mouse.get_pos()):
+                    start()
                 pygame.quit()
                 return ans

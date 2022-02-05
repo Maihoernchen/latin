d7f248741bc3ec6edc87050fbbfcb058c4bda16d
Maihoernchen <72254540+Maihoernchen@users.noreply.github.com>
2022-02-05 22:25:44 +0100

    f

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import chooser as choose
 import time
 import wrong_chooser as wrong
 
-knew = True
-
+i = 0
 question, caption, answer, questtype = choose.main()
 opt1,opt2,opt3,opt4,ans = wrong.main(questtype, answer)
 
 while True:
-    if ans != gui.main(caption, question, opt1, opt2, opt3, opt4):
-        pass
-    else:
-        question, caption, answer, questtype = choose.main()
-        opt1,opt2,opt3,opt4,ans = wrong.main(questtype, answer)
-    time.sleep(3)
+    while i != 5:
+        if ans != gui.main(caption, question, opt1, opt2, opt3, opt4):
+            pass
+        else:
+            question, caption, answer, questtype = choose.main()
+            opt1,opt2,opt3,opt4,ans = wrong.main(questtype, answer)
+            i = i+1
+    time.sleep(600)
+    i = 0
